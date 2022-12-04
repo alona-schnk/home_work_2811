90d76e094ad4ea7c39454e186f97548682846419
Альона <alonasachenko@gmail.com>
2022-12-04 13:24:22 +0200

    1) Name ok

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # home_work_2811
-Home work Python 28/11/2022
+Home work Python Basik 28/11/2022
+1) Home work number 1
+Вивести ім"я написане зірочками (*)
---
+++ home_work_1(01.12.22).py
@@ -0,0 +1,14 @@
+#import antigravity
+#import __hello__
+#from __future__ import braces
+
+
+print(("\t\t **" ), ("\t\t*"), ("\t\t\t\t  *  *  *  "), ("\t*\t\t*"), ("\t       **" ))
+print(("\t   *\t*"), ("\t\t*"), ("\t\t\t\t*\t\t  *"), ("\t**\t\t*"), ("\t\t *\t  *"))
+print(("\t  *\t\t *"), ("\t\t*"), ("\t\t\t\t*\t\t  *"), ("\t* *\t\t*"), ("\t    *\t   *"))
+print(("\t *\t\t  *"), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*  *\t*"), ("\t   *\t    *"))
+print(("\t*  *  *  * *" ), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*\t*   *"), ("\t  * *  *  *  *" ))
+print(("   *\t\t\t*"), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*\t *  *"), ("   *\t\t\t  *"))
+print(("  *\t\t\t\t *"), (" *"), ("\t\t\t\t*\t\t  *"), ("\t*\t  * *"), ("  *\t\t\t   *"))
+print((" *\t\t\t\t  *"), ("*"), ("\t\t\t\t*\t\t  *"), ("\t*\t   **"), (" *\t\t\t\t*"))
+print(("*\t\t\t\t   *"), ("*  *  *  *  * "), ("  *  *  *"), ("\t\t*\t\t*"), ("*\t\t\t\t\t *"))

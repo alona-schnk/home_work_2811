d1b81cb55accf9ad9680c2142c51f19a6d8d72ca
Альона <alonasachenko@gmail.com>
2022-12-04 13:56:37 +0200

    2) Work ok

--- home_work_1(01.12.22).py
+++ home_work_1(01.12.22).py
@@ -1,14 +1,26 @@
-#import antigravity
-#import __hello__
+import antigravity
+import __hello__
 #from __future__ import braces
 
 
-print(("\t\t **" ), ("\t\t*"), ("\t\t\t\t  *  *  *  "), ("\t*\t\t*"), ("\t       **" ))
-print(("\t   *\t*"), ("\t\t*"), ("\t\t\t\t*\t\t  *"), ("\t**\t\t*"), ("\t\t *\t  *"))
-print(("\t  *\t\t *"), ("\t\t*"), ("\t\t\t\t*\t\t  *"), ("\t* *\t\t*"), ("\t    *\t   *"))
-print(("\t *\t\t  *"), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*  *\t*"), ("\t   *\t    *"))
-print(("\t*  *  *  * *" ), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*\t*   *"), ("\t  * *  *  *  *" ))
-print(("   *\t\t\t*"), ("\t*"), ("\t\t\t\t*\t\t  *"), ("\t*\t *  *"), ("   *\t\t\t  *"))
-print(("  *\t\t\t\t *"), (" *"), ("\t\t\t\t*\t\t  *"), ("\t*\t  * *"), ("  *\t\t\t   *"))
-print((" *\t\t\t\t  *"), ("*"), ("\t\t\t\t*\t\t  *"), ("\t*\t   **"), (" *\t\t\t\t*"))
-print(("*\t\t\t\t   *"), ("*  *  *  *  * "), ("  *  *  *"), ("\t\t*\t\t*"), ("*\t\t\t\t\t *"))
+print(("\t\t **" ), ("\t\t  *"), ("\t\t\t\t *  *  *  "), ("\t*\t\t*"), ("\t       **" ))
+print(("\t   *\t*"), ("\t\t  *"), ("\t\t\t   *\t\t *"), ("\t**\t\t*"), ("\t\t *\t  *"))
+print(("\t  *\t\t *"), ("\t\t  *"), ("\t\t\t   *\t\t *"), ("\t* *\t\t*"), ("\t    *\t   *"))
+print(("\t *\t\t  *"), ("\t  *"), ("\t\t\t   *\t\t *"), ("\t*  *\t*"), ("\t   *\t    *"))
+print(("\t*  *  *  * *" ), ("\t  *"), ("\t\t\t   *\t\t *"), ("\t*\t*   *"), ("\t  * *  *  *  *" ))
+print(("   *\t\t\t*"), ("\t  *"), ("\t\t\t   *\t\t *"), ("\t*\t *  *"), ("   *\t\t\t  *"))
+print(("  *\t\t\t\t *"), ("   *"), ("\t\t\t   *\t\t *"), ("\t*\t  * *"), ("  *\t\t\t   *"))
+print((" *\t\t\t\t  *"), ("  *"), ("\t\t\t   *\t\t *"), ("\t*\t   **"), (" *\t\t\t\t*"))
+print(("*\t\t\t\t   *"), (" *  *  *  *  * "), ("    *  *  *"), ("\t*\t\t*"), ("*\t\t\t\t\t *"))
+
+print("\n\n\n")
+
+print("Escape sequences")
+print("\\a\t\t Bell (alert)")
+print("\\b\t\tBackspace")
+print("\\n\t\tNew line")
+print("\\t\t\tHorizontal tab")
+print("\\\ \t\tBackslash \ ")
+print('\\"\t\tDouble quotation mark "')
+print("\\'\t\tSingle quotation mark '")
+
